d55da56820ee8ea7156dafad9cb55103dd94873c
Rodrigo Silva <118666529+rodrigosilvacio@users.noreply.github.com>
2024-06-04 20:50:12 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,16 +1,39 @@
-document.getElementById('login-form').addEventListener('submit', function(event) {
+const mongoose = require('mongoose');
+const User = require('./models/user'); // Importe o modelo de usuário
+
+// Substitua 'sua-string-de-conexao-aqui' pela sua string de conexão do MongoDB Atlas
+const url = 'mongodb+srv://rodrigosilvapmp:hpPbWKsBX4gPe8tU@facensplay.8ezucvh.mongodb.net/';
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Erro de conexão com o banco de dados:'));
+db.once('open', function() {
+    console.log('Conectado ao banco de dados com sucesso!');
+});
+
+document.getElementById('login-form').addEventListener('submit', async function(event) {
     event.preventDefault(); // Evita o envio do formulário
 
     // Obter os valores dos campos de usuário e senha
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
-    // Simulação de validação (substituir por autenticação no MongoDB Atlas)
-    if (username === 'rodrigosilvapmp' && password === 'hpPbWKsBX4gPe8tU') {
-        // Se as credenciais estiverem corretas, redirecione para a página index.html
-        window.location.href = 'index.html';
-    } else {
-        // Se as credenciais estiverem incorretas, exiba uma mensagem de erro
-        document.getElementById('error-message').innerText = 'Usuário ou senha inválido';
-    }
-});
+    try {
+        // Enviar uma requisição para o servidor para validar as credenciais
+        const response = await fetch('/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, password })
+        });
+
+        // Verificar se a resposta foi bem-sucedida
+        if (response.ok) {
+            // Se as credenciais estiverem corretas, redirecionar para a página index.html
+            window.location.href = 'index.html';
+        } else {
+            // Se as credenciais estiverem incorretas, exibir uma mensagem de erro
+            const data = await response.json();
+            document.getElementById('
